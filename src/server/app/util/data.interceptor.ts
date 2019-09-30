8c19a0d680eb4e5c7c696bcf0c32322a4a372ce5
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-09-29 23:39:30 -0500

    tried to add logging

--- src/server/app/util/data.interceptor.ts
+++ src/server/app/util/data.interceptor.ts
@@ -10,18 +10,18 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataInterceptor implements NestInterceptor {
-intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-
-  const startTime = Date.now();
-  return next.handle().pipe(
-    map(data => {
-      const endTime = Date.now();
-      const elapsed = endTime - startTime;
-      return {
-        elapsed,
-        data: classToPlain(data)
-      };
-    })
-  );
-}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const startTime = Date.now();
+    return next.handle().pipe(
+      map(data => {
+        const endTime = Date.now();
+        const elapsed = endTime - startTime;
+        console.log(elapsed);
+        return {
+          elapsed,
+          data: classToPlain(data)
+        };
+      })
+    );
+  }
 }
